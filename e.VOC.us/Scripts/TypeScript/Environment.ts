51e28ceb0de2ddbef4db5c1318376ff609575305
Frank Nijmeijer <Frank.Nijmeijer@efocus.nl>
2015-11-18 15:40:19 +0100

    add nullcheck to Environment.ts

--- e.VOC.us/Scripts/TypeScript/Environment.ts
+++ e.VOC.us/Scripts/TypeScript/Environment.ts
@@ -3,20 +3,18 @@
         player: Player;
 
         constructor() {
-            this.player = Game.game.getCurrentPlayer();
+            
         }
 
         update() {
+            this.player = Game.game.getCurrentPlayer();
+            if (this.player != null) {
+                $('.water-middle').css("background-position-x", (-this.player.ship.rectangle.position.x + Game.game.canvas.width / 2) / 2);
+                $('.water-middle').css("background-position-y", (-this.player.ship.rectangle.position.y + Game.game.canvas.height / 2) / 2);
 
-            //Background water
-            //$('.water-background').css("background-position-x", (-this.player.ship.rectangle.position.x + Game.game.canvas.width / 2) / 4);
-            //$('.water-background').css("background-position-y", (-this.player.ship.rectangle.position.y + Game.game.canvas.height / 2) / 4);
-
-            $('.water-middle').css("background-position-x", (-this.player.ship.rectangle.position.x + Game.game.canvas.width / 2) / 2);
-            $('.water-middle').css("background-position-y", (-this.player.ship.rectangle.position.y + Game.game.canvas.height / 2) / 2);
-
-            $('.water-foreground').css("background-position-x", (-this.player.ship.rectangle.position.x + Game.game.canvas.width / 2));
-            $('.water-foreground').css("background-position-y", (-this.player.ship.rectangle.position.y + Game.game.canvas.height / 2));
+                $('.water-foreground').css("background-position-x", (-this.player.ship.rectangle.position.x + Game.game.canvas.width / 2));
+                $('.water-foreground').css("background-position-y", (-this.player.ship.rectangle.position.y + Game.game.canvas.height / 2));
+            }
         }
     }
 } 

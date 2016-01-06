be056127398e96f1a5a87d62d9e70776259abe8c
Frank <frank.nijmeijer@efocus.nl>
2016-01-06 17:53:29 +0100

    Even moar cannons

--- e.VOC.us/Game/Cannon.cs
+++ e.VOC.us/Game/Cannon.cs
@@ -1,20 +1,38 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 
 namespace e.VOC.us.Game
 {
     public class Cannon : IHasRotatableRectangle
     {
-        [JsonProperty("rectangle")]
-        public  RotatableRectangle Rectangle { get; set; }
+        private readonly Ship _ship;
+        private readonly GameState _game;
+        private const float ReloadTime = 1000;
+        private float _reloadTimer;
 
-        public Cannon(Vector2D position, float angle)
+        [JsonProperty("rectangle")] public  RotatableRectangle Rectangle { get; set; }
+
+        public Cannon(Vector2D position, float angle, Ship ship, GameState game)
         {
+            _ship = ship;
+            _game = game;
             Rectangle = new RotatableRectangle(position, 38, 20, angle);
         }
 
-        public void Update()
+        public void Update(GameTime gametime)
         {
-            
+            _reloadTimer -= gametime.ElapsedMillisecondsSinceLastUpdate;
+            _reloadTimer = Math.Max(0, _reloadTimer);
+
+            if (_ship.Player.Keyboard.IsKeyPressed(32) && (int) Math.Ceiling(_reloadTimer) == 0)
+            {
+                _reloadTimer = ReloadTime;
+                _game.CannonBalls.Add(
+                    new Cannonball(
+                        new Vector2D(Rectangle.Position.X, Rectangle.Position.Y).Add(
+                            Helper.AngleToUnitVector(Rectangle.Angle).Multiply((float) Rectangle.Width/2 + 20)),
+                        Rectangle.Angle, _game, 2000, _ship.Speed + 5, _ship.Player));
+            }
         }
     }
 }
--- e.VOC.us/Startup.cs
+++ e.VOC.us/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using System.Globalization;
+using System.Threading;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(e.VOC.us.Startup))]
@@ -9,7 +11,26 @@ namespace e.VOC.us
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            SanitizeThreadCulture();
             app.MapSignalR();
+        }
+        public static void SanitizeThreadCulture()
+        {
+            var currentCulture = CultureInfo.CurrentCulture;
+
+            // at the top of any culture should be the invariant culture,
+            // find it doing an .Equals comparison ensure that we will
+            // find it and not loop endlessly
+            var invariantCulture = currentCulture;
+            while (invariantCulture.Equals(CultureInfo.InvariantCulture) == false)
+                invariantCulture = invariantCulture.Parent;
+
+            if (ReferenceEquals(invariantCulture, CultureInfo.InvariantCulture))
+                return;
+
+            var thread = Thread.CurrentThread;
+            thread.CurrentCulture = CultureInfo.GetCultureInfo(thread.CurrentCulture.Name);
+            thread.CurrentUICulture = CultureInfo.GetCultureInfo(thread.CurrentUICulture.Name);
         }
     }
 }
